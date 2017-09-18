859d26a3b8cbfda96f45fef0c57d0a86b53a623b
Douglas Cantão <dcp.pereira@gmail.com>
2017-09-17 21:33:56 -0300

    Telas de login e cadastro
    Foi criado a tela de cadastro do sistema. Os dados ainda não estão sendo recuperados.
    As telas de login e de painel de controle foram atualizadas.

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def index(request):
+	if request.method == 'POST':
+		for key, value in request.POST.items():
+			print(key, value)
+
+		return render(request, 'paineldecontrole.html')
+
+	return render(request, 'index.html')
+
+
+def cadastro(request):
+	if request.method == 'POST':
+		for key, value in request.POST.items():
+			print(key, value)
+
+		return render(request, 'index.html')
+
+	return render(request, 'cadastro.html')
+
+def paineldecontrole(request):
+	return render(request, 'paineldecontrole.html')
