699ff2f47704e8f2d8f5ddbefeb2c994fcdee506
Douglas Cantão <dcp.pereira@gmail.com>
2017-10-20 20:30:15 -0200

    Inserção do controle de login e cadastro

--- webtour/webtour/urls.py
+++ webtour/webtour/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve as serve_static
+from webtoursite import views
 
 urlpatterns = [
 	url(r'^webtoursite/', include('webtoursite.urls')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'^conta/', include('accounts.urls')),
 ]
