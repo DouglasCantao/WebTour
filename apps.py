48bd68bbed217f22c56b4da8bbcff17ed290a125
Lívia Simões <livias.simoes@yahoo.com.br>
2017-10-28 20:11:19 -0200

    Remoção dos arquivos.

--- apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebtoursiteConfig(AppConfig):
-    name = 'webtoursite'
-
-class AccountsConfig(AppConfig):
-    name = 'accounts'
--- webtoursite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebtoursiteConfig(AppConfig):
-    name = 'webtoursite'
